787da49c9638be743bd8a4a286bc6cec27763b40
Jonas Borges <jonasrafael97@hotmail.com>
2024-03-19 15:13:52 -0300

    Update cryptography logic

--- Domain/Utils/Cryptography.cs
+++ Domain/Utils/Cryptography.cs
@@ -1,58 +1,70 @@
 ﻿using System.Security.Cryptography;
 using System.Text;
 
-namespace Domain.Utils
+namespace Domain.Utils;
+
+public static class Cryptography
 {
-    public static class Cryptography
+    private const string PEPPER_ENVIRONMENT_KEY = "PEPPER";
+    private const string SEPARATOR_TOKEN = "-";
+    private static readonly Random _random = new (Guid.NewGuid().GetHashCode());
+    
+    public static string GenerateSalt(uint size)
     {
-        private const string PEPPER_ENVIRONMENT_KEY = "PEPPER";
-        private const string SEPARATOR_TOKEN = "-";
-        private static readonly Random _random = new ();
+        StringBuilder str_build = new ();
         
-        public static string GenerateSalt(uint size)
+        for (int i = 0; i < size; i++)
         {
-            StringBuilder str_build = new ();
+            bool isNumber = _random.Next() % 2 == 0;
+            bool isUpperCase = _random.Next() % 2 != 0;
+            double seed = _random.NextDouble();
+            char value;
             
-            for (int i = 0; i < size; i++)
+            if (isNumber)
             {
-                bool isNumber = _random.Next() % 2 == 0;
-                bool isUpperCase = _random.Next() % 2 != 0;
-                double seed = _random.NextDouble();
-                char value;
-                
-                if (isNumber)
+                int shift = Convert.ToInt32(Math.Floor(10 * seed));
+                value = Convert.ToChar(shift + 48);
+            }
+            else
+            {
+                if (isUpperCase)
                 {
-                    int shift = Convert.ToInt32(Math.Floor(10 * seed));
-                    value = Convert.ToChar(shift + 48);
+                    int shift = Convert.ToInt32(Math.Floor(25 * seed));
+                    value = Convert.ToChar(shift + 65);
                 }
                 else
                 {
-                    if (isUpperCase)
-                    {
-                        int shift = Convert.ToInt32(Math.Floor(25 * seed));
-                        value = Convert.ToChar(shift + 65);
-                    }
-                    else
-                    {
-                        int shift = Convert.ToInt32(Math.Floor(25 * seed));
-                        value = Convert.ToChar(shift + 97);
-                    }
+                    int shift = Convert.ToInt32(Math.Floor(25 * seed));
+                    value = Convert.ToChar(shift + 97);
                 }
-                str_build.Append(value);
             }
-
-            return str_build.ToString();
+            str_build.Append(value);
         }
 
-        public static string EncryptPassword(string password, string salt)
-        {
-            password.ValidateStringArgumentNotNullOrEmpty(nameof(password));
-            string pepper = Environment.GetEnvironmentVariable(PEPPER_ENVIRONMENT_KEY) ?? string.Empty;
-            byte[] passwordBytes = Encoding.UTF8.GetBytes(password + salt + pepper);
-            byte[] hashBytes = SHA512.HashData(passwordBytes);
-            string hash = BitConverter.ToString(hashBytes).Replace(SEPARATOR_TOKEN, string.Empty);
+        return str_build.ToString();
+    }
 
-            return hash;
-        }
+    public static string EncryptPassword(string password, string salt)
+    {
+        password.ValidateStringArgumentNotNullOrEmpty(nameof(password));
+        
+        return Encrypt(password + EncryptSalt(salt));
+    }   
+    
+    private static string EncryptSalt(string salt)
+    {
+        salt.ValidateStringArgumentNotNullOrEmpty(nameof(salt));
+        string pepper = Environment.GetEnvironmentVariable(PEPPER_ENVIRONMENT_KEY) ?? string.Empty;
+        
+        return Encrypt(salt + pepper);
+    }
+
+    private static string Encrypt(string value)
+    {
+        byte[] saltBytes = Encoding.UTF8.GetBytes(value);
+        byte[] hashBytes = SHA512.HashData(saltBytes);
+
+        return BitConverter.ToString(hashBytes).Replace(SEPARATOR_TOKEN, string.Empty);
     }
 }
+
