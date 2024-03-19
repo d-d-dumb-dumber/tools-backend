1612fbc578ecb4600c3bd4fe0769b7411f5a6cc4
Jonas Borges <jonasrafael97@hotmail.com>
2024-03-19 13:45:33 -0300

    Add Repository

---
+++ Domain/Repositories/IUserRepository.cs
@@ -0,0 +1,9 @@
+﻿using Domain.DTOs;
+
+namespace Domain.Repositories;
+
+public interface IUserRepository
+{
+    public Task AddUser(UserDto userDto);
+    public Task<UserDto?> GetUser(string username);
+}
---
+++ Infrastructure/Repositories/UserRepository.cs
@@ -0,0 +1,22 @@
+﻿using Domain.DTOs;
+using Domain.Entities;
+using Domain.Repositories;
+using Infrastructure.DataAccess.Contexts;
+using Microsoft.EntityFrameworkCore;
+
+namespace Infrastructure.Repositories;
+
+public class UserRepository(ToolsContext context) : IUserRepository
+{
+    public async Task AddUser(UserDto userDto)
+    {
+        User userEntity = new(userDto);
+        await context.Users.AddAsync(userEntity);
+    }
+
+    public async Task<UserDto?> GetUser(string username)
+    {
+        var user = await context.Users.Where(user => user.Username == username).SingleOrDefaultAsync();
+        return user == null ? null : new UserDto(user);
+    }
+}
